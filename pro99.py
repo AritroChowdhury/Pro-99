88d089e38cec1d54f4e6aad47006cbb607d20ed0
AritroChowdhury <71551202+AritroChowdhury@users.noreply.github.com>
2021-10-27 20:10:35 +0530

    Update pro99.py

--- pro99.py
+++ pro99.py
@@ -1,22 +1,96 @@
-import time, os, and shutil1
-
-class TransferData:
-    def __init__(self,access_token):
-        self.access_token=access_token 
-
-    def upload_file(self,file_from,file_to):
-        dbx=dropbox.Dropbox(self.access_token)
-
-        f=open(file_from,'rb')
-        dbx.files_upload(f.read(),file_to)
+import os
+import shutil
+import time
 
 def main():
-    access_token='qx11iWOuCioAAAAAAAAAAeHqpgVg9_THGbAXdDUkY4y4eKfGt_yPBSksO5jmhGnA'
-    transferData=TransferData(access_token)
-    file_from=input('Enter The File Path TO Transfer-')
-    file_to =input('Enter The Full Path To Upload To Dropbox')
+	deleted_folders_count = 0
+	deleted_files_count = 0
 
-    transferData.upload_file(file_from,file_to)
-    print('File Has Been Moved')
+	path = "/PATH_TO_DELETE"
+    days = 30
 
-main()
+	
+	seconds = time.time() - (days * 24 * 60 * 60)
+
+	if os.path.exists(path):
+
+		for root_folder, folders, files in os.walk(path):
+
+			if seconds >= get_file_or_folder_age(root_folder):
+
+				remove_folder(root_folder)
+				deleted_folders_count += 1 
+
+				break
+
+			else:
+                for folder in folders:
+
+					folder_path = os.path.join(root_folder, folder)
+
+					if seconds >= get_file_or_folder_age(folder_path):
+
+						remove_folder(folder_path)
+						deleted_folders_count += 1
+
+
+				for file in files:
+
+					file_path = os.path.join(root_folder, file)
+
+					if seconds >= get_file_or_folder_age(file_path):
+
+						remove_file(file_path)
+						deleted_files_count += 1 
+
+		else:
+
+			if seconds >= get_file_or_folder_age(path):
+
+				remove_file(path)
+				deleted_files_count += 1 
+
+	else:
+
+		print(f'"{path}" is not found')
+		deleted_files_count += 1 
+
+	print(f"Total folders deleted: {deleted_folders_count}")
+	print(f"Total files deleted: {deleted_files_count}")
+
+
+def remove_folder(path):
+
+	
+	if not shutil.rmtree(path):
+
+		print(f"{path} is removed successfully")
+
+	else:
+
+		print(f"Unable to delete the "+path)
+
+
+
+def remove_file(path):
+
+	# removing the file
+	if not os.remove(path):
+
+		print(f"{path} is removed successfully")
+
+	else:
+
+		print("Unable to delete the "+path)
+
+
+def get_file_or_folder_age(path):
+
+
+	ctime = os.stat(path).st_ctime
+
+	return ctime
+
+
+if __name__ == '__main__':
+	main()
